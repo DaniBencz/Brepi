3eac5dda7054512a561d36fb298745627fe9e7df
Daniel <daniel.bencz@gmail.com>
2020-02-28 13:29:58 +0100

    prototype working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import { BrowserRouter as Router, Switch, NavLink, Route, useParams } from 'react-router-dom'
+
+function Hello({ name }: { name: string }) {
+  let { id } = useParams();
+  return (
+    <p>Hello komponens {id} {name}</p>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
-  );
+    <Router>
+      <div>
+        <p>Hello</p>
+        <NavLink to='/' className='nav-link' activeClassName='selected' exact>Home-ra</NavLink>
+        <NavLink to='/hello/1' className='nav-link' activeClassName='selected'>Hello-ra</NavLink>
+        <NavLink to='/world' className='nav-link' activeClassName='selected'>World-re</NavLink>
+        <NavLink to='/chat' className='nav-link' activeClassName='selected'>Chat-re</NavLink>
+      </div>
+      <Switch>
+        <Route path='/' exact>
+          <p>Default komponens</p>
+        </Route>
+        <Route path='/hello/:id'>
+          <Hello name='asd' />
+        </Route>
+        <Route path='/world'>
+          <p>world</p>
+        </Route>
+        <Route>
+          <p>404</p>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
